3cc0b50875ff6a47ab83a8f0961be3c534810034
Marcus Törnroth <m@rcus.se>
2014-10-09 23:39:44 +0200

    commiting test for style.php

---
+++ app/config/theme-grid.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Config-file for Anax, theme related settings, return it all as array.
+ *
+ */
+return [
+
+    /**
+     * Settings for Which theme to use, theme directory is found by path and name.
+     *
+     * path: where is the base path to the theme directory, end with a slash.
+     * name: name of the theme is mapped to a directory right below the path.
+     */
+    'settings' => [
+        'path' => ANAX_INSTALL_PATH . 'theme/',
+        'name' => 'me-grid',
+    ],
+
+    
+    /** 
+     * Add default views.
+     */
+    'views' => [
+        ['region' => 'header', 'template' => 'me/header', 'data' => [
+                'siteTitle' => "Marcus + phpmvc = sant",
+                'siteTagline' => "", //Jag och phpmvc i symbios",
+            ], 'sort' => -1],
+        ['region' => 'navbar', 'template' => [
+                'callback' => function() {
+                    return $this->di->navbar->create();
+                    },
+            ], 'data' => [], 'sort' => -1],
+        ['region' => 'footer', 'template' => 'me/footer', 'data' => [], 'sort' => -1],
+    ],
+
+
+    /** 
+     * Data to extract and send as variables to the main template file.
+     */
+    'data' => [
+
+        // Language for this page.
+        'lang' => 'sv',
+
+        // Append this value to each <title>
+        'title_append' => ' | Marcus Törnroth',
+
+        // Stylesheets
+        'stylesheets' => ['css/me-grid/style.php', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'],
+
+        // Inline style
+        'style' => null,
+
+        // Favicon
+        'favicon' => 'favicon.ico',
+
+        // Path to modernizr or null to disable
+        'modernizr' => 'js/modernizr.js',
+
+        // Path to jquery or null to disable
+        'jquery' => '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js',
+
+        // Array with javscript-files to include
+        'javascript_include' => ['js/me_fonts.js'],
+
+        // Use google analytics for tracking, set key or null to disable
+        'google_analytics' => null,
+    ],
+];
+
---
+++ webroot/theme.php
@@ -0,0 +1,82 @@
+<?php
+require __DIR__.'/config_with_app.php';
+
+$app->theme->configure(ANAX_APP_PATH . 'config/theme-grid.php');
+$app->url->setUrlType(\Anax\Url\CUrl::URL_CLEAN);
+$app->navbar->configure(ANAX_APP_PATH . 'config/navbar_me.php');
+
+$app->router->add('', function() use ($app) {
+    $app->theme->setTitle("Välkommen");
+    $content = $app->textFilter->doFilter($app->fileContent->get('me.md'), 'shortcode, markdown');
+    $byline  = $app->textFilter->doFilter($app->fileContent->get('by_marcus.md'), 'shortcode, markdown');
+    $app->views->add('me/page', [
+        'content' => $content,
+        'byline' => $byline
+    ]);
+});
+
+for($i = 1; $i <= 7; $i++) {
+    $kmom = "kmom0" . $i;
+    $app->router->add($kmom, function() use ($app, $kmom) {
+        $app->theme->setTitle(ucfirst($kmom));
+        $content = $app->textFilter->doFilter($app->fileContent->get($kmom.'.md'), 'shortcode, markdown');
+        $byline  = $app->textFilter->doFilter($app->fileContent->get('by_marcus.md'), 'shortcode, markdown');
+        $app->views->add('me/page', [
+            'content' => $content,
+            'byline' => $byline,
+        ]);
+
+        $app->dispatcher->forward([
+            'controller' => 'comment',
+            'action'     => 'view',
+            'params'     => [$kmom]
+        ]);
+
+        $app->views->add('comment/form', [
+            'mail'      => null,
+            'web'       => null,
+            'name'      => null,
+            'content'   => null,
+            'output'    => null,
+        ]);
+    }, $kmom);
+}
+
+$app->router->add('myDice', function() use ($app) {
+    $app->theme->setTitle("Spela tärning");
+    $app->theme->addStylesheet('css/dice.css');
+    $dice = new \Mos\Dice\CDice([]);
+    $app->views->add('me/dice', [
+        'dice' => $dice,
+    ]);
+
+    $app->dispatcher->forward([
+        'controller' => 'comment',
+        'action'     => 'view',
+        'params'     => ['myDice']
+    ]);
+
+    $app->views->add('comment/form', [
+        'mail'      => null,
+        'web'       => null,
+        'name'      => null,
+        'content'   => null,
+        'output'    => null,
+    ]);
+});
+
+$app->router->add('source', function() use ($app) {
+    $app->theme->setTitle("Källkod");
+    $app->theme->addStylesheet('css/source.css');
+    $source = new \Mos\Source\CSource([
+        'secure_dir' => '..', 
+        'base_dir' => '..', 
+        'add_ignore' => ['.htaccess'],
+    ]);
+    $app->views->add('me/source', [
+        'content' => $source->View(),
+    ]);
+ 
+});
+$app->router->handle();
+$app->theme->render();
