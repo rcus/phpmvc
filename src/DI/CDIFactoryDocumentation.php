d1540deaddf1144e9c2ceb7df1ed4994423dd9f2
Marcus Törnroth <m@rcus.se>
2014-11-17 15:54:38 +0100

    reset some more files

--- src/DI/CDIFactoryDocumentation.php
+++ src/DI/CDIFactoryDocumentation.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Anax\DI;
-
-/**
- * Extended factory for Anax documentation.
- *
- */
-class CDIFactoryDocumentation extends CDIFactoryDefault
-{
-   /**
-     * Construct.
-     *
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->set('documentation', function() {
-            $fc = new \Anax\Content\CFileContent();
-            $fc->setBasePath(ANAX_INSTALL_PATH . 'docs/');
-            return $fc;
-        });
-    }
-}
-=======
 <?php
 
 namespace Anax\DI;
@@ -53,4 +26,3 @@ class CDIFactoryDocumentation extends CDIFactoryDefault
         $this->theme->setBaseTitle(" - Anax documentation");
     }
 }
->>>>>>> eabc4f016b370e1e73f609a4aa3cd14d2b056f6b
